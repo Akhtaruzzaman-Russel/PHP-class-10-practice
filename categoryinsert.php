b1e41d743c5abaef10dd6537f5e3d9b9d270a06e
KaziRussel <kaziakhtaruzzaman22@gmail.com>
2024-06-22 20:29:10 +0600

    2262024830pm

--- categoryinsert.php
+++ categoryinsert.php
@@ -60,7 +60,7 @@
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-        <title>PHP Class 9</title>
+        <title>PHP Class 10</title>
     </head>
   <body>
         <div class="container py-4">
